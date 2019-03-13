55dec783c0cdc600c49c13f8736cb4798edcac9f
Jaime Spacco <jspacco@knox.edu>
2019-03-13 13:21:07 -0500

    working feverishly to get this done

--- attendance.rb
+++ attendance.rb
@@ -21,14 +21,82 @@ end
 
 # process clicker data for the whole term and compute attendance
 class Course
-  def initialize(csv = nil)
+  def initialize(csv_own = nil, csv_borrow = nil)
     # hash from session_code to number of questions
     # list of all clicker IDs for the whole term
     # hash from session_code to hash from clicker_id to number of votes
     @num_questions = Hash.new(0)
     @clicker_ids = Set.new
     @votes = Hash.new
-    @csv = csv
+    @emails = Set.new
+
+    # clicker_id => email
+    # convert to:
+    # email => set of clicker_id
+    # also add all emails to @emails
+    @own = Hash.new
+    csv_own.each do |clicker_id, email|
+      @emails.add(email)
+      if !@own.key?(email)
+        @own[email] = Set.new
+      end
+      @own[email].add(clicker_id)
+    end
+    # date => email => clicker_id
+    @borrow = csv_borrow
+    # add all the emails to our list of emails, if they aren't already there
+    @borrow.each do |date, hash|
+      for email in hash.keys
+        @emails.add(email)
+      end
+    end
+  end
+
+  def to_csv
+    out = "email,"
+    for session_code in @num_questions.keys.sort
+      out += "#{get_datetime(session_code)},"
+    end
+    for email in @emails
+      # borrow is:
+      # date => clicker_id => email
+      # own is:
+      # email => set of clicker_ids
+      out += "#{email},"
+      # go through each class period
+      for session_code in @num_questions.keys.sort
+        # get clicker_ids for this email, for this day
+        clickers = Set.new
+        # if !@own.key?(email)
+        #   STDERR.puts "NO CLICKER FOR #{email}"
+        #   next
+        # end
+        @own[email].each do |clicker_id|
+          clickers.add(clicker_id)
+        end
+        # if on this date, this email borrowed a clicker, add it to the set
+        # of clickers used by this student
+        if @borrow.key?(session_code) && @borrow[session_code].key?(email)
+          clickers.add(@borrow[session_code][email])
+        end
+        # clickers now contains all clicker_ids used by a student
+        # over the term, or on the given day
+        found = false
+        for clicker_id in clickers
+          puts @votes[session_code][clicker_id]
+          if @votes[session_code][clicker_id] > 0
+            found = true
+          end
+        end
+        if found
+          out += "1,"
+        else
+          out += "0,"
+        end
+      end
+      out += "\n"
+    end
+    return out
   end
 
   def add_question(session_code)
@@ -46,21 +114,31 @@ class Course
     @votes[session_code][clicker_id] += 1
   end
 
-  def get(session_code, clicker_id)
+  def get_vote(session_code, clicker_id)
     # if we have a CSV lookup, use it
     if @csv != nil
       if @csv.key?(clicker_id)
         return @votes[session_code][clicker_id].to_s + '<br>' +
           @csv[clicker_id]
       else
-        return @votes[session_code][clicker_id].to_s + '<br>?'
+        return @votes[session_code][clicker_id].to_s
       end
     else
       return @votes[session_code][clicker_id].to_s
     end
   end
 
-  def html(csv = nil)
+  def get_name_or_clicker(clicker_id)
+    if @own.key? clicker_id
+      return @own[clicker_id]
+    else
+      return clicker_id
+    end
+  end
+
+  def html
+    # TODO: add a way to de-anonymize, by putting in the person's name instead
+    # of just the thing
     title = "title"
     out = "<html><head><title>#{title}</title>\n"
     out += "<style>\n"
@@ -68,6 +146,7 @@ class Course
     out += "td.red {background-color: red;}\n"
     out += "</style>\n"
     out += "</head>\n"
+    out += "<body>\n"
     out += "<table border=1>\n<tr><th>clicker ID</th>\n"
     for session_code in @num_questions.keys.sort
       out += "  <th>#{get_datetime(session_code)}<br><br>#{@num_questions[session_code]} questions</th>\n"
@@ -75,22 +154,23 @@ class Course
     out += "</tr>\n"
     for clicker_id in @clicker_ids.sort
       out += "<tr>\n"
-      out += "  <td>#{clicker_id}</td>\n"
+      out += "  <td>#{get_name_or_clicker(clicker_id)}</td>\n"
       for session_code in @num_questions.keys.sort
         if @votes[session_code][clicker_id] > (3*@num_questions[session_code]/4)
-          out += "  <td class=\"green\">#{get(session_code, clicker_id)}</td>\n"
+          out += "  <td class=\"green\">#{get_vote(session_code, clicker_id)}</td>\n"
         elsif @votes[session_code][clicker_id] == 0
-          out += "  <td class=\"red\">#{get(session_code, clicker_id)}</td>\n"
+          out += "  <td class=\"red\">#{get_vote(session_code, clicker_id)}</td>\n"
         else
-          out += "  <td>#{get(session_code, clicker_id)}</td>\n"
+          out += "  <td>#{get_vote(session_code, clicker_id)}</td>\n"
         end
       end
       out += "</tr>\n"
     end
+    out += "</table></body></html>\n"
     return out
   end
 
-  def parse_XML(filename, csv)
+  def parse_XML(filename)
     page = Nokogiri::XML(open(filename))
 
     session_code = File.basename(filename, '.xml')
@@ -115,12 +195,11 @@ class Course
     end
   end
 
-  def process_course(folder, csv=nil)
-    course = Course.new
+  def process_course(folder)
     session_path = "#{folder}/SessionData/*.xml"
     Dir.glob(session_path) do |session_file|
       next if File.basename(session_file).start_with?("x")
-      parse_XML(session_file, csv)
+      parse_XML(session_file)
     end
   end
 end
@@ -129,42 +208,74 @@ end
 if __FILE__ == $0
   opts = GetoptLong.new(
     [ '--outfile', '-o', GetoptLong::REQUIRED_ARGUMENT ],
-    [ '--csvfile', '-c', GetoptLong::REQUIRED_ARGUMENT ]
+    [ '--csvregularfile', '-r', GetoptLong::REQUIRED_ARGUMENT ],
+    [ '--csvborrowfile', '-b', GetoptLong::REQUIRED_ARGUMENT ],
+    [ '--csv', '-c', GetoptLong::NO_ARGUMENT ]
   )
 
   outfile = nil
-  csv = Hash.new
-  csvfile = nil
+  csvregular = nil
+  csvborrow = nil
+  output = :html
   opts.each do |opt, arg|
     case opt
       when '--outfile'
         outfile = arg
-      when '--csvfile'
-        csvfile = arg
+      when '--csv'
+        output = :csv
+      when '--csvregularfile'
+        csvregular = Hash.new
+        # skip the header row
+        CSV.read(arg)[1 .. -1].each do |row|
+        #CSV.foreach(arg) do |row|
+          email = row[1].gsub('@knox.edu', '')
+          clicker = row[2]
+          csvregular[clicker] = email
+        end
+      when '--csvborrowfile'
+        csvborrow = Hash.new
+        # skip the header row
+        CSV.read(arg)[1 .. -1].each do |row|
+        #CSV.foreach(arg) do |row|
+          email = row[1].gsub('@knox.edu', '')
+          clicker = row[2]
+          datestr = row[3]
+          month, day, year = row[3].split('-')
+          begin
+            date = Date.new(month.to_i, day.to_i, year.to_i)
+          rescue ArgumentError
+            STDERR.puts "ROWBAR #{row}"
+            STDERR.puts "FOOBAR #{month}, #{day}, #{year}"
+            raise
+          end
+          if !csvborrow.key? datestr
+            csvborrow[datestr] = Hash.new
+          end
+          csvborrow[datestr][email] = clicker
+        end
     end
   end
 
   if ARGV.length < 1
-    puts "Usage: #{$0} [ -c <csvfile> ] [ -o <outfile> ] <course_folder>"
+    puts "Usage: #{$0} [ -r <csvregularfile> ] [ -b <csvborrowfile> ] [ -o <outfile> ] [ -c / --csv ] <course_folder>"
     exit
   end
   classdir = ARGV.shift
 
-  if csvfile != nil
-    #CSV.foreach(csv, headers:true) do |row|
-    CSV.foreach(csvfile) do |row|
-      email = row[1].gsub('@knox.edu', '')
-      clicker = row[2]
-      csv[clicker] = email
-    end
+  course = Course.new(csvregular, csvborrow)
+  course.process_course(classdir)
+
+  result = nil
+  if output == :html
+    result = course.html
+  elsif output == :csv
+    result = course.to_csv
   end
 
-  course = Course.new(csv)
-  course.process_course(classdir, csv)
-
+  # TODO: Output can be CSV, anonymous CSV, HTML, anonymous HTML
   if outfile == nil
-    puts course.html
+    puts result
   else
-    File.open(outfile, 'w') { |file| file.write(course.html) }
+    File.open(outfile, 'w') { |file| file.write(result) }
   end
 end
